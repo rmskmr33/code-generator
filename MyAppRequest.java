f113711f68657f701dd054fb4eeb35398a688f1c
Rameshkumar <srivan.ramesh@gmail.com>
2025-12-13 09:02:49 -0500

    Create MyAppRequest.java

---
+++ MyAppRequest.java
@@ -0,0 +1,44 @@
+public class MyAppRequest {
+
+    private String appid;
+    private String groupId;
+    private String artifactId;
+    private String packageName;
+    private String version;
+
+    public String getAppid() {
+        return appid;
+    }
+    public void setAppid(String appid) {
+        this.appid = appid;
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getArtifactId() {
+        return artifactId;
+    }
+    public void setArtifactId(String artifactId) {
+        this.artifactId = artifactId;
+    }
+
+    public String getPackageName() {
+        return packageName;
+    }
+    public void setPackageName(String packageName) {
+        this.packageName = packageName;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+    public void setVersion(String version) {
+        this.version = version;
+    }
+}
+✅ 2. Up
